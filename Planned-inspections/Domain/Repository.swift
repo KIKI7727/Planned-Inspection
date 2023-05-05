ce575c15d584fa3b1ad0c6fdb7edba59c2d9c62d
dongyu.cai <458498007@qq.com>
2023-05-05 15:25:46 +0800

    feat: add list view data structure

--- Planned-inspections/Domain/Repository.swift
+++ Planned-inspections/Domain/Repository.swift
@@ -13,3 +13,22 @@ struct DaysModel {
   let dayofMonth: String
   let planned: Bool
 }
+
+struct PlannedRespository {
+  var title: String = ""
+  var inspectionCount: Int = 0
+  var plannedCard: [PlannedCardInfo] = []
+}
+
+struct PlannedCardInfo {
+  var timeslot: String = ""
+  var inspection: [InspectionInfo] = []
+}
+
+struct InspectionInfo {
+  var title: String = ""
+  var address: String = "19.05/116  Bathurst Street, Sydney, NSW 2000"
+  var bathrooms: Int = 0
+  var bedrooms: Int = 0
+  var parkingSpaces: Int = 0
+}
